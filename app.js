72ff7effdaeac3570d17362111c2189ee78cafcb
lxzv9999 <1138830257@qq.com>
2017-11-22 11:19:11 +0800

    Signed-off-by: lxzv9999 <1138830257@qq.com>

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿var http=require("http")
+var fs=require('fs')
+var  querystring= require('querystring')
+http.createServer(function(req,res){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    var str="";
+    var data=[]
+    req.on("data",function(data){
+        console.log(data)
+        str+=data;
+    })
+    req.on("end",function(err){
+        console.log(str)
+        var json=querystring.parse(str)
+        console.log(json)
+        var data = fs.readFileSync('app.txt','utf-8')
+        data = JSON.parse(data)
+        data.push({user:json.user,pass:json.pass})
+        fs.writeFileSync('app.txt',JSON.stringify(data));
+        res.write(JSON.stringify(data))
+        res.end();
+
+
+
+
+    })
+}).listen(9999)
